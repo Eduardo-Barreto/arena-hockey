f8295ac7ce175dab4be373d5b7dc39ac83b9d840
Eduardo Barreto <eduardopontobarreto@gmail.com>
2026-03-16 00:18:28 -0300

    feat(input): scalable per-robot player input system

---
+++ scripts/local_player_input.gd
@@ -0,0 +1,14 @@
+class_name LocalPlayerInput
+extends Node
+
+@export var player_index := 1
+
+var _drive := DriveController.new()
+
+
+func _physics_process(_delta: float) -> void:
+	var prefix := "p%d_" % player_index
+	var axis_a := Input.get_axis(prefix + "back", prefix + "forward")
+	var axis_b := Input.get_axis(prefix + "left", prefix + "right")
+	var wheels := _drive.process(axis_a, axis_b)
+	(get_parent() as Robot).receive_input(wheels.x, wheels.y)
