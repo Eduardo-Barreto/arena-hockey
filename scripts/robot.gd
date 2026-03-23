6282e1a427a29087121b116af11e2cf31fdaa5dc
Eduardo Barreto <eduardopontobarreto@gmail.com>
2026-03-22 23:04:37 -0300

    feat(robot): add max speed limiter to prevent wall clipping

--- scripts/robot.gd
+++ scripts/robot.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 
 @export var max_engine_force := 8.0
 @export var torque_multiplier := 1.0
+@export var max_speed := 2.0
 @export var wheel_separation := 0.16
 @export var team_color := Color.RED
 
@@ -56,7 +57,9 @@ func _physics_process(delta: float) -> void:
 	forward /= traction
 
 	var net_force := (left_force + right_force) / 2.0
-	apply_central_force(forward * net_force * traction)
+	var speed := linear_velocity.length()
+	if speed < max_speed or net_force * linear_velocity.dot(forward) <= 0.0:
+		apply_central_force(forward * net_force * traction)
 
 	var yaw_torque := (right_force - left_force) * wheel_separation / 2.0 * torque_multiplier
 	apply_torque(Vector3.UP * yaw_torque * traction)
